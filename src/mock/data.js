52e6d8dce43e544b6315c5fad59365f6900afc73
Suniel <Suniel@users-MacBook-Pro.local>
2021-10-21 11:21:20 +0700

    message

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,26 +2,26 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Sunny', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Suniel Jolly',
+  subtitle: "JS Developer",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
+  img: 'profile.jpeg',
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://github.com/sunieljolly', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -33,62 +33,44 @@ export const projectsData = [
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+    repo: '', // if no repo, the button will not show up
+  },  
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'sunieljolly@googlemail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
+    /* {
       id: nanoid(),
       name: 'twitter',
       url: '',
-    },
-    {
+    }, */
+/*     {
       id: nanoid(),
       name: 'codepen',
       url: '',
-    },
-    {
+    }, */
+/*     {
       id: nanoid(),
       name: 'linkedin',
       url: '',
-    },
+    }, */
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/sunieljolly',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
